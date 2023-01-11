7cfa00edd57e34e8bb350b3ed6dd940537fa2a6f
akbaroke <akbarmuhammad833@gmail.com>
2023-01-11 20:59:42 +0700

    update

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('m-bca', 'root', '', {
-  host: 'localhost',
+const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
 });
 
--- index.js
+++ index.js
@@ -15,9 +15,11 @@ try {
   console.log(error);
 }
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => console.log('server running at port:5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('server running at port:5000'));
