0b199ab93e9a8cd69b10f2f3e1f98171aec14fc1
akbaroke <akbarmuhammad833@gmail.com>
2023-01-13 00:59:18 +0700

    update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions ={
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
