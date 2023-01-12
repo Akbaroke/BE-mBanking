0584669e75e786c9b4674d913665047723798f49
akbaroke <akbarmuhammad833@gmail.com>
2023-01-12 07:22:17 +0700

    update

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ try {
 
 // { credentials: true, origin: 'https://fe-m-banking.vercel.app/' }
 // { credentials: true, origin: 'https://fe-m-banking.vercel.app/' }
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
